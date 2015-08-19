8367ffbf0a2b669fc7581e94a30828529b13db75
igor <igor.sv.n@gmail.com>
2015-08-19 14:35:08 +0300

    Add image validation constraint to AbstractImage::$file.

--- Entity/Image/AbstractImage.php
+++ Entity/Image/AbstractImage.php
@@ -14,6 +14,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
@@ -62,6 +63,7 @@ abstract class AbstractImage
      * @var \Symfony\Component\HttpFoundation\File\File
      *
      * @Vich\UploadableField(mapping="darvin_image", fileNameProperty="filename")
+     * @Assert\Image
      */
     private $file;
 
