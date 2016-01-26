a0d62d8087baca24579a7ee499f04adb6a4c4f2e
igor <igor.sv.n@gmail.com>
2016-01-26 10:37:08 +0300

    Change AbstractImage::$sizes fetch mode from "EAGER" to "EXTRA_LAZY".

--- Entity/Image/AbstractImage.php
+++ Entity/Image/AbstractImage.php
@@ -43,7 +43,7 @@ abstract class AbstractImage
     /**
      * @var \Darvin\ImageBundle\Entity\Image\Size[]|\Doctrine\Common\Collections\Collection
      *
-     * @ORM\OneToMany(targetEntity="Darvin\ImageBundle\Entity\Image\Size", mappedBy="image", cascade={"persist", "remove"}, orphanRemoval=true, fetch="EAGER")
+     * @ORM\OneToMany(targetEntity="Darvin\ImageBundle\Entity\Image\Size", mappedBy="image", cascade={"persist", "remove"}, orphanRemoval=true, fetch="EXTRA_LAZY")
      * @Assert\Valid
      */
     private $sizes;
