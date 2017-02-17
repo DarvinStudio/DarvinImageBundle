b20b32ca4d709e31f643b15f56683b5fdc012da3
igor <igor.sv.n@gmail.com>
2017-02-17 11:49:29 +0300

    Autoload app configs.

--- DependencyInjection/DarvinImageExtension.php
+++ DependencyInjection/DarvinImageExtension.php
@@ -13,15 +13,17 @@ namespace Darvin\ImageBundle\DependencyInjection;
 use Darvin\Utils\DependencyInjection\ConfigInjector;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DarvinImageExtension extends Extension
+class DarvinImageExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -47,6 +49,23 @@ class DarvinImageExtension extends Extension
             'url_builder',
         ] as $resource) {
             $loader->load($resource.'.yml');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $fileLocator = new FileLocator(__DIR__.'/../Resources/config/app');
+
+        foreach ([
+            'liip_imagine',
+            'vich_uploader',
+        ] as $extension) {
+            if ($container->hasExtension($extension)) {
+                $container->prependExtensionConfig($extension, Yaml::parse(file_get_contents($fileLocator->locate($extension.'.yml')))[$extension]);
+            }
         }
     }
 }
