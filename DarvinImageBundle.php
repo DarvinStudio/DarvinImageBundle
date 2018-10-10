3e4772e44878dcd414b36d2adb4a83c3a7b78379
Igor Nikolaev <igor.sv.n@gmail.com>
2018-10-10 12:20:19 +0300

    Update version constant values.

--- DarvinImageBundle.php
+++ DarvinImageBundle.php
@@ -20,7 +20,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class DarvinImageBundle extends Bundle
 {
-    const MAJOR_VERSION = 6;
+    public const MAJOR_VERSION = 7;
 
     /**
      * {@inheritdoc}
