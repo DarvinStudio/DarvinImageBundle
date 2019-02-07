cd539c86a597ea436b141fd2989819700e0bb2e8
Igor Nikolaev <igor.sv.n@gmail.com>
2019-02-07 16:13:46 +0300

    Enable strict types.

--- DarvinImageBundle.php
+++ DarvinImageBundle.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2015, Darvin Studio
+ * @copyright Copyright (c) 2015-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
@@ -25,7 +25,7 @@ class DarvinImageBundle extends Bundle
     /**
      * {@inheritdoc}
      */
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
