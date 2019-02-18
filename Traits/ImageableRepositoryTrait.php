186d648b80757bac8191360b124a71e7b60500d7
Igor Nikolaev <igor.sv.n@gmail.com>
2019-02-18 12:03:42 +0300

    Remove inner join method from imageable repository trait.

--- Traits/ImageableRepositoryTrait.php
+++ Traits/ImageableRepositoryTrait.php
@@ -26,38 +26,7 @@ trait ImageableRepositoryTrait
      *
      * @return self
      */
-    protected function innerJoinImage(QueryBuilder $qb, ?string $locale = null, bool $addSelect = true, string $join = 'o.image', string $alias = 'image')
-    {
-        $translationsAlias = sprintf('%s_translations', $alias);
-
-        $qb
-            ->innerJoin($join, $alias)
-            ->innerJoin(sprintf('%s.translations', $alias), $translationsAlias);
-
-        if (null !== $locale) {
-            $qb
-                ->andWhere(sprintf('%s.locale = :locale', $translationsAlias))
-                ->setParameter('locale', $locale);
-        }
-        if ($addSelect) {
-            $qb
-                ->addSelect($alias)
-                ->addSelect($translationsAlias);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param \Doctrine\ORM\QueryBuilder $qb        Query builder
-     * @param string|null                $locale    Locale
-     * @param bool                       $addSelect Whether to add select
-     * @param string                     $join      Join
-     * @param string                     $alias     Alias
-     *
-     * @return self
-     */
-    protected function leftJoinImage(QueryBuilder $qb, ?string $locale = null, bool $addSelect = true, string $join = 'o.image', string $alias = 'image')
+    protected function joinImage(QueryBuilder $qb, ?string $locale = null, bool $addSelect = true, string $join = 'o.image', string $alias = 'image')
     {
         $translationsAlias = sprintf('%s_translations', $alias);
 
