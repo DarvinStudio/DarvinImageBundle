1243f1e57e57041cb18267caaced67c3ca5c34aa
Igor Nikolaev <igor.sv.n@gmail.com>
2020-11-17 11:29:38 +0300

    Inherit file entity class.

--- Entity/Image/AbstractImage.php
+++ Entity/Image/AbstractImage.php
@@ -11,127 +11,43 @@
 namespace Darvin\ImageBundle\Entity\Image;
 
 use Darvin\ContentBundle\Traits\TranslatableTrait;
+use Darvin\FileBundle\Entity\AbstractFile;
 use Darvin\ImageBundle\Validation\Constraints as DarvinImageAssert;
-use Darvin\Utils\Mapping\Annotation\Clonable;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\Validator\Constraints as Assert;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
- * Image abstract implementation
+ * Image
  *
  * @ORM\Entity
- * @ORM\InheritanceType("SINGLE_TABLE")
- * @ORM\Table(name="image")
- *
- * @Clonable\Clonable(copyingPolicy="ALL")
- *
- * @Vich\Uploadable
  *
  * @method string|null getAlt()
  * @method string|null getTitle()
  */
-abstract class AbstractImage
+abstract class AbstractImage extends AbstractFile
 {
     use TranslatableTrait;
 
-    public const PROPERTY_FILE = 'file';
-
-    private const VECTOR_EXTENSIONS = [
+    protected const VECTOR_EXTENSIONS = [
         'svg',
     ];
 
     /**
-     * @var int
+     * @var int|null
      *
-     * @ORM\Column(type="integer", unique=true)
-     * @ORM\GeneratedValue
-     * @ORM\Id
+     * @ORM\Column(type="integer", nullable=true)
      */
-    private $id;
-
-    /**
-     * @var bool
-     *
-     * @ORM\Column(type="boolean")
-     */
-    private $enabled;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column
-     *
-     * @Assert\NotBlank(groups={"AdminUpdateProperty"})
-     */
-    private $name;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column
-     */
-    private $extension;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column
-     */
-    private $filename;
+    protected $width;
 
     /**
      * @var int|null
      *
      * @ORM\Column(type="integer", nullable=true)
      */
-    private $width;
+    protected $height;
 
     /**
-     * @var int|null
-     *
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private $height;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(type="integer")
-     *
-     * @Gedmo\SortablePosition
-     */
-    private $position;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(type="datetime")
-     */
-    private $updatedAt;
-
-    /**
-     * @var \Symfony\Component\HttpFoundation\File\File|null
-     *
-     * @Vich\UploadableField(mapping="darvin_image", fileNameProperty="filename")
-     *
-     * @DarvinImageAssert\DarvinImage
-     */
-    private $file;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->enabled   = true;
-        $this->updatedAt = new \DateTime();
-    }
-
-    /**
-     * @return string
+     * {@inheritDoc}
      */
     public function __toString(): string
     {
@@ -147,7 +63,7 @@ abstract class AbstractImage
             return $title;
         }
 
-        return $this->name;
+        return parent::__toString();
     }
 
     /**
@@ -159,9 +75,30 @@ abstract class AbstractImage
     }
 
     /**
-     * @return string
+     * @DarvinImageAssert\DarvinImage
+     *
+     * @return \Symfony\Component\HttpFoundation\File\File|null
      */
-    abstract public static function getUploadDir(): string;
+    public function getFile(): ?File
+    {
+        return parent::getFile();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setFile(?File $file): AbstractFile
+    {
+        if (null !== $file) {
+            $size = @getimagesize($file->getPathname());
+
+            if (is_array($size)) {
+                list($this->width, $this->height) = $size;
+            }
+        }
+
+        return parent::setFile($file);
+    }
 
     /**
      * @return string|null
@@ -181,191 +118,5 @@ abstract class AbstractImage
     public function isVector(): bool
     {
         return in_array($this->extension, self::VECTOR_EXTENSIONS);
-    }
-
-    /**
-     * @param \Symfony\Component\HttpFoundation\File\File|null $file file
-     *
-     * @return AbstractImage
-     */
-    public function setFile(?File $file): AbstractImage
-    {
-        if (null !== $file) {
-            $size = @getimagesize($file->getPathname());
-
-            if (is_array($size)) {
-                list($this->width, $this->height) = $size;
-            }
-
-            $this->updatedAt = new \DateTime();
-        }
-
-        $this->file = $file;
-
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isEnabled(): ?bool
-    {
-        return $this->enabled;
-    }
-
-    /**
-     * @param bool $enabled enabled
-     *
-     * @return AbstractImage
-     */
-    public function setEnabled(?bool $enabled): AbstractImage
-    {
-        $this->enabled = $enabled;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name name
-     *
-     * @return AbstractImage
-     */
-    public function setName(?string $name): AbstractImage
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getExtension(): ?string
-    {
-        return $this->extension;
-    }
-
-    /**
-     * @param string $extension extension
-     *
-     * @return AbstractImage
-     */
-    public function setExtension(?string $extension): AbstractImage
-    {
-        $this->extension = $extension;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getFilename(): ?string
-    {
-        return $this->filename;
-    }
-
-    /**
-     * @param string $filename filename
-     *
-     * @return AbstractImage
-     */
-    public function setFilename(?string $filename): AbstractImage
-    {
-        $this->filename = $filename;
-
-        return $this;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getWidth(): ?int
-    {
-        return $this->width;
-    }
-
-    /**
-     * @param int|null $width width
-     *
-     * @return AbstractImage
-     */
-    public function setWidth(?int $width): AbstractImage
-    {
-        $this->width = $width;
-
-        return $this;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getHeight(): ?int
-    {
-        return $this->height;
-    }
-
-    /**
-     * @param int|null $height height
-     *
-     * @return AbstractImage
-     */
-    public function setHeight(?int $height): AbstractImage
-    {
-        $this->height = $height;
-
-        return $this;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPosition(): ?int
-    {
-        return $this->position;
-    }
-
-    /**
-     * @param int $position position
-     *
-     * @return AbstractImage
-     */
-    public function setPosition(?int $position): AbstractImage
-    {
-        $this->position = $position;
-
-        return $this;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getUpdatedAt(): \DateTime
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * @return \Symfony\Component\HttpFoundation\File\File|null
-     */
-    public function getFile(): ?File
-    {
-        return $this->file;
     }
 }
