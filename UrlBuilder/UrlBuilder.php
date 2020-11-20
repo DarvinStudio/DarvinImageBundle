0768a9e12deffa2614d01f595b5ac7e53da3b1b8
Igor Nikolaev <igor.sv.n@gmail.com>
2020-11-20 08:35:01 +0300

    Update copyrights.

--- UrlBuilder/UrlBuilder.php
+++ UrlBuilder/UrlBuilder.php
@@ -2,7 +2,7 @@
 /**
  * @author    Lev Semin     <lev@darvin-studio.ru>
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2015-2019, Darvin Studio
+ * @copyright Copyright (c) 2015-2020, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
